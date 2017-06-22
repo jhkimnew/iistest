31570d4c846cf5eee1f3aa8c52c97615360e1db6
Jeong Hwan Kim <jhkim@windows.microsoft.com>
2017-06-22 12:08:50 -0700

    Updated

--- test-iis.ps1
+++ test-iis.ps1
@@ -2,7 +2,6 @@
 
 .VERSION 1.0.0
 
-<<<<<<< HEAD
 .GUID 2f16e59f-b261-41ce-bccc-ac64fa47330a
       
 .AUTHOR Jeong Hwan Kim
@@ -18,39 +17,8 @@
 
 .DESCRIPTION
     Test IIS.
-=======
-.GUID 1b4ee806-349d-3108-9126-c7692d602ebc
-
-.AUTHOR Jeong Hwan Kim
-
-.COMPANYNAME Microsoft Corporation
-
-.COPYRIGHT (C) Microsoft Corporation. All rights reserved.
-
-.TAGS IIS
-
-.LICENSEURI 
-
-.PROJECTURI 
-
-.ICONURI 
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS 
-
-.EXTERNALSCRIPTDEPENDENCIES 
-
-.RELEASENOTES
-
->>>>>>> 1a5e9e48c778338e77a3f8ddf2799ad034c3b51a
-#>
-
-<# 
 .Synopsis
-   Show an ASCII text representation of a namespace tree
-.DESCRIPTION 
- Script to test IIS web sites
+   TBD
 .EXAMPLE
 PS> test-iis -Feature CORS
 
